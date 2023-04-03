5c969e220db55d98ab4463bc310432d873e957c9
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-04-03 18:55:04 +0700

    Rename

--- Core/Entities/OrderProducts.cs
+++ Core/Entities/OrderProducts.cs
@@ -1,6 +1,6 @@
 ﻿namespace Core.Entities {
     public class OrderProducts {
-        public int Id { get; set; }
+        public int OrderId { get; set; }
 
         public int ProductId { get; set; }
 
